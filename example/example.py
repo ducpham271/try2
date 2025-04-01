40a8d852a7651660688746f6b3ac86583cd34813
Duc <duc23mse23145@fsb.edu.vn>
2025-04-01 20:13:02 +0700

    commit

--- example/example.py
+++ example/example.py
@@ -15,13 +15,13 @@ drive_folder_id = st.secrets["DRIVE_FOLDER_ID"]  # Get from Streamlit secrets
 service = build('drive', 'v3', credentials=creds)
 vietnam_timezone = pytz.timezone('Asia/Ho_Chi_Minh')
 
-def save_ggdrive(audio, _name, _year_of_birth):
+def save_ggdrive(audio, _name, _gender, _year_of_birth):
     st.audio(audio.export().read())
     
     utc_now = datetime.datetime.now().replace(tzinfo=pytz.utc)
     vietnam_now = utc_now.astimezone(vietnam_timezone)
     timestamp = vietnam_now.strftime("%Y%m%d_%H%M%S")  # Format: YYYYMMDD_HHMMSS
-    filename = f"0_{_name}_{_year_of_birth}_{timestamp}.wav"
+    filename = f"0_{_name}_{_gender}_{_year_of_birth}_{timestamp}.wav"
 
     audio.export(filename, format="wav")
     print(filename)
@@ -70,14 +70,18 @@ col1, col2 = st.columns([1, 2])
 with col1:
     st.write("Họ tên:")
 with col2:
-    # name = st.text_input("", st.session_state["name"], key="name_input", label_visibility="collapsed")
     name = st.text_input("", key="name_input", label_visibility="collapsed")
+
+col7, col8 = st.columns([1, 2])
+with col7:
+    st.write("Giới tính:")
+with col8:
+    gender = st.radio("", ['Nam', 'Nữ'], label_visibility="collapsed")
 
 col3, col4 = st.columns([1, 2])
 with col3:
     st.write("Năm sinh:")
 with col4:
-    # year_of_birth = st.number_input("", value=st.session_state["yob"], min_value=1900, max_value=2025, step=1, key="yob", label_visibility="collapsed")
     year_of_birth = st.number_input("", value=1960, min_value=1900, max_value=2025, step=1, key="yob", label_visibility="collapsed")
 
 st.markdown("---")
@@ -85,16 +89,15 @@ st.markdown("NỘI DUNG GHI ÂM:")
 st.write("1. Phát âm nguyên âm “A” dài và lâu nhất có thể (lần 1)")
 audio1 = audiorecorder("Ghi âm", "Ngừng ghi âm", custom_style={"backgroundColor": "lightblue"}, key="ghiam1")
 if len(audio1) > 0:
-    save_ggdrive(audio1, name, year_of_birth)
+    save_ggdrive(audio1, name, gender, year_of_birth)
 st.write("2. Phát âm nguyên âm “A” dài và lâu nhất có thể (lần 2)")
 audio2 = audiorecorder("Ghi âm", "Ngừng ghi âm", custom_style={"backgroundColor": "lightblue"}, key="ghiam2")
 if len(audio2) > 0:
-    save_ggdrive(audio2, name, year_of_birth)
+    save_ggdrive(audio2, name, gender, year_of_birth)
 st.write("3. Phát âm nguyên âm “A” dài và lâu nhất có thể (lần 3)")
 audio3 = audiorecorder("Ghi âm", "Ngừng ghi âm", custom_style={"backgroundColor": "lightblue"}, key="ghiam3")
 if len(audio3) > 0:
-    save_ggdrive(audio3, name, year_of_birth)
+    save_ggdrive(audio3, name, gender, year_of_birth)
 st.write("")
 st.markdown("---")
 st.write("")
-# st.write("Lời cảm ơn: Xin cảm ơn ông/bà cô/chú anh/chị Cộng Đồng PARKINTON VIỆT NAM, đặc biệt là anh admin Tung Mix vì đã hỗ trợ em thực hiện đồ án này!")
