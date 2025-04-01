7138e879ca228f05a293265e89eb0238a96362fa
Duc <duc23mse23145@fsb.edu.vn>
2025-04-01 21:22:18 +0700

    commit

--- example/example.py
+++ example/example.py
@@ -21,7 +21,7 @@ def save_ggdrive(audio, _name, _gender, _year_of_birth):
     utc_now = datetime.datetime.now().replace(tzinfo=pytz.utc)
     vietnam_now = utc_now.astimezone(vietnam_timezone)
     timestamp = vietnam_now.strftime("%Y%m%d_%H%M%S")  # Format: YYYYMMDD_HHMMSS
-    filename = f"0_{_name}_{_gender}_{_year_of_birth}_{timestamp}.wav"
+    filename = f"0_{_name}_{_gender}_{_year_of_birth}_{timestamp}_a.wav"
 
     audio.export(filename, format="wav")
     print(filename)
